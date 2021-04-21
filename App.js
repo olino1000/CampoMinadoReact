555b33061b5d7df38e72454d1ae11d4bbe20b357
Vinícius Olino <olino1000@gmail.com>
2021-04-20 22:03:44 -0300

    Commit 20/04

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
-import React, {Component} from 'react';
-import params from './src/param'
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params'
+//import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Text styles={styles.welcome}>Iniciando Mines!!!</Text>
-        <Text styles={styles.instructions}>Tamanho do campo: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-      </SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>Tamanho da grade:
+         {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+
+        {/* <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged />
+        <Field flagged opened /> */}
+      </View>
     )
   }
 }
@@ -20,19 +54,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome:{
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    fontSize: 15,
-    textAlign: 'center',
-    margin: 10
-  }
-});
+    justifyContent: 'flex-end',
 
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
+})
