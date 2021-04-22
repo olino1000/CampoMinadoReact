fb431d36ac515a320aea9a1918bad0ab6110777a
Vinícius Olino <olino1000@gmail.com>
2021-04-22 19:38:35 -0300

    Commit 22/04

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,12 @@ export default props => {
     return (
         <View style={styles.container}>
             <View style={styles.flagContainer}>
-                <TouchableOpacity onPress={props.onFlagPress}
-                    style={styles.flagButton}>
+                <TouchableOpacity onPress={props.onFlagPress} style={styles.flagButton}>
                     <Flag bigger />
                 </TouchableOpacity>
                 <Text style={styles.flagsLeft}>= {props.flagsLeft}</Text>
             </View>
-            <TouchableOpacity style={styles.button}
-                onPress={props.onNewGame}>
+            <TouchableOpacity style={styles.button} onPress={props.onNewGame}>
                 <Text style={styles.buttonLabel}>Novo Jogo</Text>
             </TouchableOpacity>
         </View>
